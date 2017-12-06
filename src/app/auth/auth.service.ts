1acdb68fd3c7dc46f1992e367d45a0a2d5eaa1db
Jin Hu <jinhu.coder@gmail.com>
2017-12-07 01:33:32 +1100

    add logout function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthService {
   token: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   signupUser(email: string, password: string) {
   	firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -27,9 +28,18 @@ export class AuthService {
     );
   }
 
+  signoutUser() {
+    firebase.auth().signOut();
+    this.token = null;
+  }
+
   getToken() {
     firebase.auth().currentUser.getToken()
       .then(token => this.token = token);
     return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
   }
 }
